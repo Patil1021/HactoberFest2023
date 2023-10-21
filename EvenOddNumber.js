f7b04c35c6901c0ed2a1466a793e9454ea3d1f1a
Patil Lakshminarayanamma <47653644+Patil1021@users.noreply.github.com>
2023-10-21 18:58:21 +0530

    Create EvenOddNumber.js

---
+++ EvenOddNumber.js
@@ -0,0 +1,10 @@
+// Returns true if n is 
+// even, else odd 
+function isEven(n) { 
+	return (n % 2 == 0); 
+} 
+
+// Driver code 
+let n = 101; 
+
+isEven(n) ? console.log("Even") : console.log("Odd");
